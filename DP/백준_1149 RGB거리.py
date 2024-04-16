0dde5b2a7e1eafa171a3d8729e4cf545cccb3a9e
SANGEUN LEE <sangeun0205@naver.com>
2024-04-16 11:27:08 +0900

    이코테DP_개미전사

--- DP/백준_1149 RGB거리.py
+++ DP/백준_1149 RGB거리.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 
 arr=[]
@@ -5,42 +7,28 @@ arr=[]
 for i in range(n):
     arr.append(list(map(int,input().split())))
     
-min_arr = []
+dp = []
 for _ in range(n):
-    min_arr.append([0, 0, 0])
+    dp.append([0, 0, 0])
 
-min_arr[0][0] = arr[0][0]
-min_arr[0][1] = arr[0][1]
-min_arr[0][2] = arr[0][2]
+dp[0][0] = arr[0][0]
+dp[0][1] = arr[0][1]
+dp[0][2] = arr[0][2]
 
 for i in range(1, n):
     
-    if min_arr[i-1][0]+arr[i][2] <= min_arr[i-1][1]+arr[i][2]:
-        min_arr[i][2]= min_arr[i-1][0]+arr[i][2]
-    else: min_arr[i][2] = min_arr[i-1][1]+arr[i][2]
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2])+arr[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2])+arr[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1])+arr[i][2]
     
-    if min_arr[i-1][1]+arr[i][0] <= min_arr[i-1][2]+arr[i][0]:
-        min_arr[i][0]= min_arr[i-1][1]+arr[i][0]
-    else: min_arr[i][0] = min_arr[i-1][2]+arr[i][0]
+    '''
+    if dp[i-1][0]+arr[i][1] <= dp[i-1][2]+arr[i][1]:
+        dp[i][1]= dp[i-1][0]+arr[i][1]
+    else: dp[i][1] = dp[i-1][2]+arr[i][1]
     
-    if min_arr[i-1][0]+arr[i][1] <= min_arr[i-1][2]+arr[i][1]:
-        min_arr[i][1]= min_arr[i-1][0]+arr[i][1]
-    else: min_arr[i][1] = min_arr[i-1][2]+arr[i][1]
+    ↑ 이런 형식에서 간단하게 수정한 뒤에 시간 반으로 줄음. 
     
-print(min(min_arr[n-1]))
-
-
-
-
-# if arr[0][0]+arr[i][2] <= arr[0][1]+arr[1][2]:
-#         min2=arr[0][0]+arr[1][2]
-#     else: min2 = arr[0][1]+arr[1][2]
-
-#     if arr[0][0]+arr[1][1] <= arr[0][2]+arr[1][1]:
-#         min1=arr[0][0]+arr[1][2]
-#     else: min1 = arr[0][2]+arr[1][1]
-
-#     if arr[0][1]+arr[1][0] <=arr[0][2]+arr[1][0]:
-#         min0 = arr[0][1]+arr[1][0]
-#     else: min0 = arr[0][2]+arr[1][0]
+    '''
+    
+print(min(dp[n-1]))
     
