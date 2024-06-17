c0f8243c4eb79259571a8e09bc3ade2c96c9fcee
Sangeun <sangeun0205@naver.com>
2024-06-17 22:53:28 +0900

    24.06.17

--- Graph/최소신장트리.py
+++ Graph/최소신장트리.py
@@ -7,9 +7,18 @@
 프림, 크루스칼 알고리즘 등으로 구현한다.
 '''
 
-G1 = None
+G1= None
 nameAry = ['춘천', '서울', '속초', '대전', '광주', '부산']
 춘천, 서울, 속초, 대전, 광주, 부산 = 0, 1, 2, 3, 4, 5
+
+gSize = 6
+G1 = graph(gSize)
+G1.graph[춘천][서울] = 10; G1.graph[춘천][속초] = 15
+G1.graph[서울][춘천] = 10; G1.graph[서울][속초] = 40; G1.graph[서울][대전] = 1; G1.graph[서울][광주] = 50
+G1.graph[속초][춘천] = 15; G1.graph[속초][서울] = 40; G1.graph[속초][대전] = 12
+G1.graph[대전][서울] = 11; G1.graph[대전][속초] = 12; G1.graph[대전][광주] = 20; G1.graph[대전][부산] = 30
+G1.graph[광주][서울] = 50; G1.graph[광주][대전] = 20; G1.graph[광주][부산] = 25
+G1.graph[부산][대전] = 30; G1.graph[부산][광주] = 25
 
 edgeAry = []
 for i in range(gSize) :
